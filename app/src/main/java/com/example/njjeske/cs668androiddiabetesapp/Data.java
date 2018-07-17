9465f3773aaa1d98982d9262e9d842fb2b491129
Kim <kpham16@gmail.com>
2018-07-17 16:53:28 -0500

    Redid home page to card view, working on new Menu layout, added empty java class

---
+++ app/src/main/java/com/example/njjeske/cs668androiddiabetesapp/Data.java
@@ -0,0 +1,13 @@
+package com.example.njjeske.cs668androiddiabetesapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Data extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data);
+    }
+}
---
+++ app/src/main/java/com/example/njjeske/cs668androiddiabetesapp/Home.java
@@ -0,0 +1,61 @@
+package com.example.njjeske.cs668androiddiabetesapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+
+public class Home extends AppCompatActivity implements View.OnClickListener {
+    private CardView bglCard, foodCard, exerciseCard, medicineCard, dataCard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        // defining Cards
+        bglCard = findViewById(R.id.bgl_card);
+        foodCard = findViewById(R.id.food_card);
+        exerciseCard = findViewById(R.id.exercise_card);
+        medicineCard = findViewById(R.id.medicine_card);
+        dataCard = findViewById(R.id.data_card);
+
+        // Add click listeners to the card
+        bglCard.setOnClickListener(this);
+        foodCard.setOnClickListener(this);
+        exerciseCard.setOnClickListener(this);
+        medicineCard.setOnClickListener(this);
+        dataCard.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+
+        switch (v.getId()) {
+            case R.id.bgl_card:
+                i = new Intent(this, BGL.class);
+                startActivity(i);
+                break;
+            case R.id.food_card:
+                i = new Intent(this, Food.class);
+                startActivity(i);
+                break;
+            case R.id.exercise_card:
+                i = new Intent(this, Exercise.class);
+                startActivity(i);
+                break;
+            case R.id.medicine_card:
+                i = new Intent(this, Medicine.class);
+                startActivity(i);
+                break;
+            case R.id.data_card:
+                i = new Intent(this, Data.class);
+                startActivity(i);
+                break;
+            default:
+                break;
+        }
+    }
+}
